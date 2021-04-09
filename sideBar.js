86a427e7e11904e692bd81761ba589c3160d48c4
Mahesh <maheshsangeet0@gmail.com>
2021-04-09 14:48:08 +0530

    done

---
+++ sideBar.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.querySelector(".side-bar").style.width = "200px";
+  document.querySelector(".side-bar").style.height = "100vh" ;
+  document.querySelector(".side-bar").style.padding = "30px 50px" ;
+
+}
+
+function closeNav() {
+  document.querySelector(".side-bar").style.width = "0";
+  document.querySelector(".side-bar").style.padding= "0";
+}
